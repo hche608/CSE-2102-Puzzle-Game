330b07c9d39559f2413e67ff941fd10dd2aea4e3
Hao CHEN <hao.3.chen@uconn.edu>
2015-02-16 12:25:04 -0500

    add player class

---
+++ PuzzleGame/src/puzzleGame/Player.java
@@ -0,0 +1,32 @@
+package puzzleGame;
+
+public class Player {
+	private String playerName;
+	private int score;
+	
+	public Player(){
+		this.playerName = "Alex";
+		this.score = 0;
+	}
+	
+	public Player(String playerName){
+		this.playerName = playerName;
+		this.score = 0;
+	}
+	
+	public void setScore(int score){
+		this.score = score;
+	}
+	
+	public void setPlayerName(String playerName){
+		this.playerName = playerName;
+	}
+	
+	public int getScore(){
+		return score;
+	}
+	
+	public String getPlayerName(){
+		return playerName;
+	}
+}
