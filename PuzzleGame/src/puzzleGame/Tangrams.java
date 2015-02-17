7ff61b10fe66c9a1e6fe1213bd9caa9bfad7777b
Hao CHEN <hao.3.chen@uconn.edu>
2015-02-17 14:04:39 -0500

    add demo menubar

--- PuzzleGame/src/puzzleGame/Tangrams.java
+++ PuzzleGame/src/puzzleGame/Tangrams.java
@@ -1,20 +1,24 @@
 package puzzleGame;
 import javax.swing.*;
 
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
 
 public class Tangrams extends JFrame {
-	private Label _label;
-	
-<<<<<<< HEAD
+	//private Label _label;
+	private final PlayArea playArea;
+	private Player player;
+
 /*	
 	public void Tangrams() {
-=======
+
 	
 	public Tangrams() {
->>>>>>> 2d1c4c638e15e82c0bae9ae4f2443d2d48db1b10
+
 		this.setSize(300, 300);
 		this.setVisible(true);
 		
@@ -25,10 +29,57 @@ public class Tangrams extends JFrame {
     public Tangrams() {
         setTitle("Tangram Game");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        //Container cp = getContentPane();
+        Container cp = getContentPane();
+        
+        playArea = new PlayArea();
+        cp.add(playArea, BorderLayout.CENTER);
+        
+        // Set up menu bar:
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+        
+        JMenu fileMenu = new JMenu("File");
+        
+        menuBar.add(fileMenu);
+        
+        JMenuItem newgame = fileMenu.add("New Game");
+        newgame.addActionListener(new ActionListener(){
+        	@Override
+            public void actionPerformed(ActionEvent e) {
+        		player.setPlayerName(JOptionPane.showInputDialog("Please input your name: "));
+        	}
+        });
+		JMenuItem pause = fileMenu.add("Pause Game");
+		JMenuItem resume = fileMenu.add("Resume Game");
+		JMenuItem scores = fileMenu.add("Scores");
+
+        
+        fileMenu.addSeparator();
+
+        JMenuItem ExitGame = new JMenuItem("Exit", KeyEvent.VK_X);
+        ExitGame.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, InputEvent.CTRL_MASK));
+        ExitGame.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            	if (JOptionPane.showConfirmDialog(playArea,
+                        "Really exit from Game?",
+                        "Please confirm",
+                        JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
+            		System.exit(0);
+            	}
+            	
+            }
+        });
+        fileMenu.add(ExitGame);
+        
+        JMenu helpMenu = new JMenu("Help");
+        helpMenu.setMnemonic(KeyEvent.VK_H);
+        menuBar.add(helpMenu);
         
         
-        setPreferredSize(new Dimension(300, 300));
+        
+        // Set initial dimension and pack:
+        setPreferredSize(new Dimension(800, 600));
         pack();
     }
 
